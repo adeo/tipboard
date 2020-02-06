0c3d2070102282a863ab22bcbcf4e474448e6aee
Joris Lefait <jlefait@lamobilery.fr>
2020-02-06 10:45:06 +0100

    fic sendDataToTipboard in src/sensors/utils.py

---
+++ src/sensors/matomo_utils.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import json
+from statistics import mean
+
+import requests
+import logging
+
+base_url = "http://benchmark-analytics-open.apps.op.acp.adeo.com/"
+token = "0b44547f5f0c9d526b46247c65aeef57"
+
+
+# logging.basicConfig(level=logging.DEBUG)
+def basicMatomoRequest(method="", query=None):
+    if query is None:
+        query = {}
+    params = dict()
+    params['token_auth'] = token
+    params['module'] = "API"
+    params['method'] = method
+    params["period"] = "range"
+    params["date"] = "2020-01-13,2020-03-10"
+    params["format"] = "JSON"
+    params["idSite"] = "1"
+    params.update(query)
+    return requests.get(base_url, params=params)
+
+
+def getValueFromActionID(segment, id):
+    params = dict()
+    params["segment"] = segment
+    params["idSubtable"] = id
+    response = basicMatomoRequest(method="Events.getNameFromActionId", query=params)
+    if response.status_code == 200:
+        responsesList = []
+        for values in response.json():
+            responsesList.append(values["label"])
+        return responsesList
+    raise
+
+
+def getActionsIdByCategory(segment):
+    params = dict()
+    params["segment"] = segment
+    response = basicMatomoRequest(method="Events.getAction", query=params)
+    if response.status_code == 200:
+        idSubList = []
+        for action in response.json():
+            idSubList.append({k: action[k] for k in ('label', 'idsubdatatable')})
+        return idSubList
+    raise
+
+
+def getCategories():
+    params = dict()
+    response = basicMatomoRequest(method="Events.getCategory", query=params)
+    if response.status_code == 200:
+        categoryList = []
+        for category in response.json():
+            categoryList.append({k: category[k] for k in ('label', 'segment')})
+        return categoryList
+    raise
+
+
+def valueFromAction(action):
+    test = []
+    categoryList = getCategories()
+    for segment in categoryList:
+        x = dict()
+        x["device"] = segment["label"]
+        idSubList = getActionsIdByCategory(segment["segment"])
+        for id in idSubList:
+            if id["label"] == action:
+                x["values"] = [float(item) for item in getValueFromActionID(segment["segment"], id["idsubdatatable"])]
+                x["avg"] = mean(x["values"])
+        if "values" in x:
+            test.append(x)
+    return test
+
+
+def getDevices():
+    return [label["label"] for label in getCategories()]
---
+++ src/sensors/sensor_benchmark.py
@@ -0,0 +1,63 @@
+import time
+
+from src.sensors.matomo_utils import valueFromAction, getDevices
+from src.sensors.utils import end, sendDataToTipboard
+from src.tipboard.app.properties import COLOR_TAB
+
+
+def updateNormChartTipBoard(bench, tile, isTest=False):
+    if not "values" in bench[0]:
+        return
+    datasetLength = len(bench)
+    data = dict()
+    data['title'] = dict(display=False)
+    data['datasets'] = list()
+    for index in range(datasetLength):
+        data['datasets'].append(
+            dict(label=bench[index]["device"],
+                 data=bench[index]["values"],
+                 borderColor=COLOR_TAB[index]))
+    tipboardAnswer = sendDataToTipboard(data=data, tile_template='norm_chart', tile_id=tile, isTest=isTest)
+    end(title=f'{tile} -> {tile}', start_time=time.time(), tipboardAnswer=tipboardAnswer, TILE_ID=tile)
+
+
+def updateListingTipBoard(list, tile, isTest=False):
+    data = {'items': list}
+    tipboardAnswer = sendDataToTipboard(data=data, tile_template='norm_chart', tile_id=tile, isTest=isTest)
+    end(title=f'{tile} -> {tile}', start_time=time.time(), tipboardAnswer=tipboardAnswer, TILE_ID=tile)
+
+
+def updateCPUTipBoard(isTest=False):
+    print(f'CPU function')
+    cpu_bench = valueFromAction("ackleyBenchmark")
+    # print(f'CPU = {cpu_bench}')
+    updateNormChartTipBoard(cpu_bench, 'cpu', isTest)
+
+
+def updateGPUTipBoard(isTest=False):
+    print(f'GPU function')
+    gpu_bench = valueFromAction("scroll")
+    # print(f'GPU = {gpu_bench}')
+    updateNormChartTipBoard(gpu_bench, 'gpu', isTest)
+
+
+def updateNetworkTipBoard(isTest=False):
+    print(f'Network function')
+    network_bench = valueFromAction("dowloadFile")
+    # print(f'network function result : {network_bench}')
+    updateNormChartTipBoard(network_bench, 'network', isTest)
+
+
+def updateDevicesTipBoard(isTest=False):
+    list_of_devices = getDevices()
+    updateListingTipBoard(list_of_devices, 'list_devices', isTest)
+
+
+def sonde_bench(isTest=False):
+    updateCPUTipBoard(isTest)
+    updateGPUTipBoard(isTest)
+    # updateNetworkTipBoard(isTest)
+    # updateDevicesTipBoard(isTest)
+
+
+sonde_bench()
